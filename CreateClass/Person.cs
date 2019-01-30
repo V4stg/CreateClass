1c98d37ddf8ab8ede7b201055b82f6bfa81f6486
V4stg <lukacsb@gmail.com>
2019-01-30 17:47:38 +0100

    initial commit; added Person class

---
+++ CreateClass/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string BirthDate { get; set; }
+
+        public enum String { Male, Female}
+
+        public override string ToString()
+        {
+            return $"{Name} was born on {BirthDate}.";
+        }
+    }
+}
--- CreateClass/Program.cs
+++ CreateClass/Program.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CreateClass
+﻿namespace CreateClass
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Person firstPerson = new Person();
+            System.Console.WriteLine("Enter your name:");
+            firstPerson.Name = System.Console.ReadLine();
+            System.Console.WriteLine("Enter your birth date:");
+            firstPerson.BirthDate = System.Console.ReadLine();
+
+            System.Console.WriteLine(firstPerson.ToString());
+            System.Console.ReadKey(); // to wait for user to quit
         }
     }
 }
